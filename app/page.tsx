839d3105ce5072f6f6da1d37f9ac4dcd29f81e4b
Max Paulus 🥪 <maxpaulu@amazon.com>
2025-09-07 20:41:40 -0700

    add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
@@ -41,7 +39,7 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted. Try creating a new todo man dude.
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
